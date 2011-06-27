1cc191acaed5ba72652f0949f2ad17d4c36c0d26
binom <binomod@gmail.com>
2011-06-28 00:07:52 +0400

    initial cimmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f06b5f7ecf4df2891956043a97dd1d42516d74f81cf402a6545fb37db90907ca1bee9bfca873fdf168a3ec9d8084ad7aba9f62799964644152f3706455531d98'
